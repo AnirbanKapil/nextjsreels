69ecc4dab99f9b41b87e300f7e79084e6d281c5b
Anirban Kapil <anirbankapil@gmail.com>
2025-02-14 11:32:07 +0530

    mongoDB connection setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,30 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URL = process.env.MONGODB_URL!;
 
 
 if(!MONGODB_URL){
-    throw new Error("Plz define mongoDB URL in env file")
+    throw new Error("Plz define mongoDB URL in env file");
+};
+
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn : null , promise : null}
+}
+
+export async function connectionToDatabase () {
+    if(cached){
+        return cached.conn
+    };
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands : true,
+            maxPoolSize : 10
+        }
+    };
+
+    cached.promise = mongoose.connect()
 }
