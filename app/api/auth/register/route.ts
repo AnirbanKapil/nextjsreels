8a4675102103e2bd386e7d123b4f2e3d329b6055
Anirban Kapil <anirbankapil@gmail.com>
2025-02-15 22:08:19 +0530

    backend setup

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest , NextResponse } from "next/server";
+import { connectionToDatabase } from "@/lib/db";
+import User from "@/models/User"; 
+
+
+
+export async function POST (request : NextRequest) {
+    
+}
