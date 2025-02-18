9cdabef926c8a11676d7e355eebdf6958a157a5a
Anirban Kapil <anirbankapil@gmail.com>
2025-02-18 17:18:34 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import next from "next";
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+
+export default withAuth(
+    function middleware () {
+        return NextResponse.next()
+    },
+    {
+        callbacks : {
+            authorized : ({token,req}) => {
+                 const {pathname} = req.nextUrl;
+                 
+                //  auth related routes
+                 if(
+                    pathname.startsWith("/api/auth") ||
+                    pathname === "/login" ||
+                    pathname === "/register"
+                ){
+                    return true
+                 }
+
+                //  public routes
+                if(pathname === "/" || pathname.startsWith("/api/videos")){
+                    return true
+                }
+
+                return !!token
+            }
+        }
+    }
+) 
+
+
+export const config = {
+    matcher : [
+        
+    ]
+}
